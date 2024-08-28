5efa1d369df4b4243b8aec1cd70c390c7b5a085a
aloshy.ΞTH <aloshyakasoto@gmail.com>
2024-08-28 09:57:10 +0400

    Moved data from file to ENV

--- src/utils/assignee.ts
+++ src/utils/assignee.ts
@@ -1,21 +1,17 @@
-import fs from 'fs'
-import path from 'path'
-
 let assigneeUsernameMap: { [key: string]: string } = {}
 
-// Load the JSON file if it exists
-const assigneeMapPath = path.resolve(process.cwd(), '@/../data/assigneeTelegramIds.json')
+// Load the mapping from the environment variable
+const assigneeMapEnv = process.env.ASSIGNEE_TELEGRAM_IDS
 
-if (fs.existsSync(assigneeMapPath)) {
+if (assigneeMapEnv) {
   try {
-    const fileContent = fs.readFileSync(assigneeMapPath, 'utf-8')
-    assigneeUsernameMap = fileContent ? JSON.parse(fileContent) : {}
+    assigneeUsernameMap = JSON.parse(assigneeMapEnv)
   } catch (error) {
-    console.error('Error reading or parsing assigneeTelegramIds.json:', error)
+    console.error('Error parsing ASSIGNEE_TELEGRAM_IDS from ENV:', error)
     assigneeUsernameMap = {}
   }
 } else {
-  console.warn('assigneeTelegramIds.json does not exist. Using an empty mapping.')
+  console.warn('ASSIGNEE_TELEGRAM_IDS environment variable does not exist. Using an empty mapping.')
 }
 
 /**
